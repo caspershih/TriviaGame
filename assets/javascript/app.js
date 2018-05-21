83966977c5a3aab3610b4798ebdfd21d2f447183
Casper Shih <casshih@gmail.com>
2018-05-21 14:25:43 -0700

    adding

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,35 @@
 // timer
+var timer;
 
-// make the flash cards responsive
+
+
 
 // slideshow
 var slides = 1;
 showSlides(slide);
 
-// Next page and previous page
+function plusSlides(n) {
+    showSlides(slides +=n);
+}
+
+function currentslide(n) {
+    showslides(slides = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slides = 1}
+    if (n < 1) {slides = slides.length}
+    for (i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slide-1].style.display = "block";
+    dots[slide-1].className += "active";
+}
+
+// make the flash cards responsive
